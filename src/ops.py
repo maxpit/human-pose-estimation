7b718671b16e09989a4a129a6cbdec99bad98b08
ge52wew <max.pittner@tum.de>
2019-08-01 16:29:51 +0200

    finished ops.py for submission

--- src/ops.py
+++ src/ops.py
@@ -1,248 +1,125 @@
-"""
-TF util operations.
-"""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-from sklearn.neighbors import NearestNeighbors
-import numpy as np
 
 
-def keypoint_l1_loss(kp_gt, kp_pred, scale=1., name="kp_l1_loss"):
-    """
+"""
+    joint_reprojection_loss
     computes: \Sum_i [0.5 * vis[i] * |kp_gt[i] - kp_pred[i]|] / (|vis|)
     Inputs:
-      kp_gt  : N x K x 3
-      kp_pred: N x K x 2
-    """
+        kp_gt  : N x K x 3
+            - N is batch size
+            - K is number of joints/keypoints (19)
+            - 3 values for x,y position and 1/0 whether the keypoint is visible or not
+        kp_pred: N x K x 2
+            - N is batch size
+            - K is number of joints/keypoints (19)
+            - 2 values for x,y
+    Outputs:
+        summed L1 loss between predicted and ground truth joints for given mesh
+"""
+def joint_reprojection_loss(kp_gt, kp_pred, scale=1., name="joint_reprojection_loss"):
+
     with tf.name_scope(name):
         kp_gt = tf.reshape(kp_gt, (-1, 3))
         kp_pred = tf.reshape(kp_pred, (-1, 2))
 
+        # visibilities of joints
         vis = tf.expand_dims(tf.cast(kp_gt[:, 2], tf.float32), 1)
-        #print("kp_gt, kp_pred: ", kp_gt[:,2], kp_pred, vis)
-        #res = tf.losses.absolute_difference(kp_gt[:, :2], kp_pred, weights=vis)
+
+        # only consider distance if keypoint is visible
         res = tf.compat.v1.losses.absolute_difference(kp_gt[:, :2], kp_pred, weights=vis)
-        #TODO: ...losses.absolute_difference Deprecated. Replace with tf2.0 fuction --> see warning.
+
         return res
 
 
-#TODO this belongs in ops.py?
-def get_3d_loss(self, Rs, shape, joints):
-    """
-    Rs is N x 24 x 3*3 rotation matrices of pose
-    Shape is N x 10
-    joints is N x 19 x 3 joints
-
-    Ground truth:
-    self.poseshape_loader is a long vector of:
-       relative rotation (24*9)
-       shape (10)
-       3D joints (14*3)
-    """
-    Rs = tf.reshape(Rs, [self.batch_size, -1])
-    params_pred = tf.concat([Rs, shape], 1, name="prep_params_pred")
-    # 24*9+10 = 226
-    gt_params = self.poseshape_loader[:, :226]
-    loss_poseshape = self.e_3d_weight * compute_3d_loss(
-        params_pred, gt_params, self.has_gt3d_smpl)
-    # 14*3 = 42
-    gt_joints = self.poseshape_loader[:, 226:]
-    pred_joints = joints[:, :14, :]
-    # Align the joints by pelvis.
-    pred_joints = align_by_pelvis(pred_joints)
-    pred_joints = tf.reshape(pred_joints, [self.batch_size, -1])
-    gt_joints = tf.reshape(gt_joints, [self.batch_size, 14, 3])
-    gt_joints = align_by_pelvis(gt_joints)
-    gt_joints = tf.reshape(gt_joints, [self.batch_size, -1])
-
-    loss_joints = self.e_3d_weight * compute_3d_loss(
-        pred_joints, gt_joints, self.has_gt3d_joints)
-
-    return loss_poseshape, loss_joints
-
-def compute_3d_loss(params_pred, params_gt, has_gt3d):
-    """
-    Computes the l2 loss between 3D params pred and gt for those data that has_gt3d is True.
-    Parameters to compute loss over:
-    3Djoints: 14*3 = 42
-    rotations:(24*9)= 216
-    shape: 10
-    total input: 226 (gt SMPL params) or 42 (just joints)
-
+"""
+    find_nearest_neighbors
+    Computes nearest neighbors between two sets of 2-dim vectors.
+    To avoid for loops a distance matrix is computed.
     Inputs:
-      params_pred: N x {226, 42}
-      params_gt: N x {226, 42}
-      # has_gt3d: (N,) bool
-      has_gt3d: N x 1 tf.float32 of {0., 1.}
-    """
-    with tf.name_scope("3d_loss", [params_pred, params_gt, has_gt3d]):
-        weights = tf.expand_dims(tf.cast(has_gt3d, tf.float32), 1)
-        res = tf.losses.mean_squared_error(
-            params_gt, params_pred, weights=weights) * 0.5
-        return res
+        A:    num_A x 2
+        B:    num_B x 2
+    Outputs:
+        indices of nearest neighbors of A to B and vice versa
+"""
+def find_nearest_neighbors(A, B):
 
-
-def align_by_pelvis(joints):
-    """
-    Assumes joints is N x 14 x 3 in LSP order. #TODO: Check whether LSP order = our order !!!
-    Then hips are: [3, 2]
-    Takes mid point of these points, then subtracts it.
-    """
-    with tf.name_scope("align_by_pelvis", [joints]):
-        left_id = 3
-        right_id = 2
-        pelvis = (joints[:, left_id, :] + joints[:, right_id, :]) / 2.
-        return joints - tf.expand_dims(pelvis, axis=1)
-
-def find_nearest_neighbors_naive(A, B):
-    B = tf.cast(B, dtype=tf.int64)
-    ind_BA = []
-    for i in range(B.shape[0]):
-        # print(i)
-        distance = tf.reduce_sum(tf.abs(tf.add(A, tf.negative(B[i, :]))), reduction_indices=1)
-        # print(distance)
-        # Prediction: Get min distance index (Nearest neighbor)
-        ind = tf.argmin(distance, 0)
-        # with tf.Session() as sess:
-        #    print("dist: ", distance.eval())
-        #    print("pred: ", ind.eval())
-        # print(ind)
-        ind_BA.append(ind)
-    ind_BA = tf.stack(ind_BA)
-
-    ind_AB = []
-    #for i in range(A.shape[0]):
-    for i in range(100):
-        # print(i)
-        distance = tf.reduce_sum(tf.abs(tf.add(B, tf.negative(A[i, :]))), reduction_indices=1)
-        # print(distance)
-        # Prediction: Get min distance index (Nearest neighbor)
-        ind = tf.argmin(distance, 0)
-        # with tf.Session() as sess:
-        #    print("dist: ", distance.eval())
-        #    print("pred: ", ind.eval())
-        # print(ind)
-        ind_AB.append(ind)
-    ind_AB = tf.stack(ind_AB)
-
-    return ind_AB, ind_BA
-
-
-def find_nearest_neighbors_advanced(A, B):
+    # compute L2 distance matrix between A and B
     dists = tf.scalar_mul(-2., tf.matmul(A, B, transpose_b=True)) + \
             tf.expand_dims(tf.reduce_sum(tf.square(A), 1), 1) + \
             tf.expand_dims(tf.reduce_sum(tf.square(B), axis=1), 0)
+
+    # get the indices of matrices A and B where distance is smallest (nearest neighbor indices)
     ind_AB = tf.argmin(dists, 1)
     ind_BA = tf.argmin(dists, 0)
+
     return ind_AB, ind_BA
 
 
+"""
+    bidirectional_dist
+    Computes bidirectional distance between two sets of 2-dim vectors.
+    Inputs:
+        A:    num_A x 2
+        B:    num_B x 2
+    Outputs:
+        bidirectional distance
+"""
 def bidirectional_dist(A, B):
-    # get nearest neighbors B of A
-    #if (A = tenso)
 
-    #A_np = np.copy(A)
-    #B_np = np.copy(B)
-    #print(A_np)
-    #print(B_np)
-    #B_const = tf.constant(B)
-    #print("A: ", A)
-    #print("B: ", B)
-
-    #with tf.Session() as sess:
-    #tf.initialize_all_variables().run()
-    #print("A.shape: ", A.eval(session=sess).shape)
-    #print("B.shape: ", B.eval(session=sess).shape)
-    #print("eval A: ", A.eval(session=sess))
-    #print("eval B: ", B.eval(session=sess))
-    #nbrs_B = NearestNeighbors(n_neighbors=1,
-    #                          algorithm='ball_tree').fit(sess.run(A))
-    #distances_BA, ind_BA = nbrs_B.kneighbors(sess.run(B))
-
-    # get nearest neighbors A of B
-    #nbrs_A = NearestNeighbors(n_neighbors=1,
-    #                          algorithm='ball_tree').fit(sess.run(B))
-    #distances_AB, ind_AB = nbrs_A.kneighbors(sess.run(A))
-    #ind_AB, ind_BA = find_nearest_neighbors_naive(A, B)
-    ind_AB, ind_BA = find_nearest_neighbors_advanced(A, B) #tf.cast(A, dtype=tf.float32), B)
+    # get nearest neighbors between A and B
+    ind_AB, ind_BA = find_nearest_neighbors(A, B)
 
     # compute distances
+    # distance of matrix B to its nearest neighbors in A
+    # we take L2 loss here
     dist_BA = tf.norm(B - tf.gather(A, ind_BA), axis=1)
     #dist_AB = tf.norm(tf.to_float(A) - tf.to_float(tf.gather(B, ind_AB)), axis=1)
-    dist_AB = tf.reduce_sum(tf.abs(A - tf.gather(B, ind_AB)))
+
+    # distance of matrix A to its nearest neighbors in B
+    # we take L1 loss here
+    dist_AB = tf.reduce_sum(tf.abs(A - tf.gather(B, ind_AB)), axis=1)
+
+    # sum up all dists
     summed_dist_BA = tf.reduce_sum(dist_BA)
     summed_dist_AB = tf.reduce_sum(dist_AB)
-    # print(distances_BA.sum()+distances_AB.sum())
-    # print(tf.math.add(summed_dist_BA, summed_dist_AB).eval())
 
-    return tf.math.add(summed_dist_BA, summed_dist_AB), summed_dist_BA, summed_dist_AB
+    return tf.math.add(summed_dist_BA, summed_dist_AB)
 
 
-def mesh_reprojection_loss(silhouette_gt, silhouette_pred, batch_size, name="mesh_reprojection_loss"):
-    """
-    ADL4CV
-    Computes bidirectional distance between ground truth silhouette and predicted silhouette
+"""
+    mesh_reprojection_loss
+    Computes mesh reprojection loss for a given batch.
     Inputs:
-        silhouette_gt:      ### N x P (num pixels of silhouette) x 2 -->   try with N*P x 3
-        silhouette_pred:    N x 6890 (num vertices) x 2
-    """
-    with tf.name_scope(name):
-        #K = silhouette_gt.shape[1]
-      #  silhouette_gt = tf.reshape(silhouette_gt, (-1, 2))
-      #  silhouette_pred = tf.reshape(silhouette_pred, (-1, 2))
-      #  sil_gt_np = silhouette_gt.eval()
-      #  sil_pred_np = silhouette_pred.eval()
-        #print("silhouette_gt!!!!!!!!!!!!!!!", silhouette_gt)
-        #if(batch_size==1):
-            #loss = bidirectional_dist(tf.gather_nd(silhouette_gt,
-             #                                       tf.where(tf.equal(silhouette_gt[:,0], 0)))[:, 1:], silhouette_pred[0,:,:])
-        #else:
-        #loss = tf.Variable(0, name="mesh_reprojection_loss", trainable=False, dtype=tf.float32) # variable?
-        #ab_all = tf.Variable(0., trainable=False) # variable?
-        #ba_all = tf.Variable(0., trainable=False) # variable?
-        for i in range(batch_size):
-            silhouette_points_gt = tf.stack([tf.gather_nd(silhouette_gt,
-                                            tf.where(tf.equal(silhouette_gt[:, 0], i)))[:, 2],
-                                             tf.gather_nd(silhouette_gt,
-                                                          tf.where(tf.equal(silhouette_gt[:, 0], i)))[:, 1]
-                                             ], axis=1)
+        silhouette_gt:      N*Pi x 3 
+                                - first column contains index of image in batch
+                                - second and third column are x- and y-image coordinates
+                                - Pi is number of silhouette pixels in image i
+        silhouette_pred:    N x 6890 (num vertices mesh) x 2
+    Outputs:
+        Mesh reprojection loss on batch
+"""
+def mesh_reprojection_loss(silhouette_gt, silhouette_pred, batch_size, name="mesh_reprojection_loss"):
 
-            bi_loss, _, _ = bidirectional_dist(silhouette_points_gt,
-                               silhouette_pred[i, :, :])
+    with tf.name_scope(name):
+        for i in range(batch_size):
+
+            # get ground truth silhouette of ith image in batch in shape [Pi x 2]
+            silhouette_gt_x = tf.gather_nd(silhouette_gt, tf.where(tf.equal(silhouette_gt[:, 0], i)))[:, 2]
+            silhouette_gt_y = tf.gather_nd(silhouette_gt, tf.where(tf.equal(silhouette_gt[:, 0], i)))[:, 1]
+            silhouette_points_gt = tf.stack([silhouette_gt_x, silhouette_gt_y], axis=1)
+
+            # compute the bidirectional distance of image i
+            bi_loss = bidirectional_dist(silhouette_points_gt, silhouette_pred[i, :, :])
             bi_loss_scaled = bi_loss/(silhouette_gt.shape[1] +
                                       silhouette_pred.shape[1])
+
+            # sum up losses for all images
             if i == 0:
                 loss = bi_loss_scaled
             else:
                 loss = loss + bi_loss_scaled
-            #loss = tf.add(loss, bi_loss_scaled)
-            #ab_all = tf.math.add(ab_all, ab)
-            #ba_all = tf.math.add(ba_all, ba)
-        return loss#, ab_all, ba_all, tf.stack([tf.gather_nd(silhouette_gt,
-                    #                        tf.where(tf.equal(silhouette_gt[:, 0], 0)))[:, 2],
-                     #                        tf.gather_nd(silhouette_gt,
-                      #                                    tf.where(tf.equal(silhouette_gt[:, 0], 0)))[:, 1]
-                       #                      ], axis=1), silhouette_pred[0, :, :]
-
-##################
-### Test functions
-##################
-# N = 10
-# num_keypoints = 19
-# random_nr = 20
-# num_vertices = 6890
-#
-# keypoints_gt = np.random.rand(N*num_keypoints, 3)
-# for i in range(N*num_keypoints):
-#     keypoints_gt[i, 0] = i%random_nr
-#
-# keypoints_pred = np.random.rand(N, num_vertices, 2)
-#
-# print("loss: ")
-# loss = mesh_reprojection_loss(keypoints_gt, keypoints_pred, N)
-#
-# print("loss.shape: ", loss)
+        return loss
